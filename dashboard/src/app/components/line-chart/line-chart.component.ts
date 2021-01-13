0669be562a055ef12d458f552afd166e1b6d4e2c
Mischa <michael.hoser@outlook.com>
2021-01-13 22:01:56 +0100

    Chart with zoom

--- dashboard/src/app/components/line-chart/line-chart.component.ts
+++ dashboard/src/app/components/line-chart/line-chart.component.ts
@@ -9,17 +9,58 @@ import { EChartsOption } from 'echarts';
 export class LineChartComponent implements OnInit {
 
   chartOption: EChartsOption = {
+    title: {
+      text: 'CO² Konzentration (ppm)'
+    },
     xAxis: {
       type: 'category',
-      data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+      // data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
     },
     yAxis: {
       type: 'value',
     },
+
+    dataZoom: [{
+      startValue: 0
+    }, {
+      type: 'inside'
+    }],
+
+    visualMap: {
+      top: 10,
+      right: 10,
+      pieces: [{
+        gt: 0,
+        lte: 850,
+        color: '#096'
+      }, {
+        gt: 850,
+        lte: 1000,
+        color: 'orange'
+      }, {
+        gt: 1000,
+        color: 'red',
+        // label: 'kritisch (>1000)',
+      }],
+      outOfRange: {
+        color: '#999'
+      }
+    },
     series: [
       {
-        data: [820, 932, 901, 934, 1290, 1330, 1320],
+        // data: [820, 932, 901, 934, 1290, 1330, 1320],
         type: 'line',
+        data: [
+          ['2019-10-10', 200],
+          ['2019-10-11', 400],
+          ['2019-10-12', 650],
+          ['2019-10-13', 500],
+          ['2019-10-14', 250],
+          ['2019-10-15', 300],
+          ['2019-10-16', 450],
+          ['2019-10-17', 300],
+          ['2019-10-18', 100]
+        ]
       },
     ],
   };
