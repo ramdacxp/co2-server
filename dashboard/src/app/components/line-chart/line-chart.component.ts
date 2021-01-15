dd4cd506951c2ef51deacbaf940c4440736e3d18
Mischa <michael.hoser@outlook.com>
2021-01-15 21:44:34 +0100

    Update data on change

--- dashboard/src/app/components/line-chart/line-chart.component.ts
+++ dashboard/src/app/components/line-chart/line-chart.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { EChartsOption } from 'echarts';
 import { ChartDataService } from 'src/app/services/chart-data.service';
 
@@ -8,8 +8,6 @@ import { ChartDataService } from 'src/app/services/chart-data.service';
   styleUrls: ['./line-chart.component.scss']
 })
 export class LineChartComponent implements OnInit {
-
-  private echartsInstance: any;
 
   private _dataSource: string;
 
@@ -22,25 +20,15 @@ export class LineChartComponent implements OnInit {
     return this._dataSource;
   }
 
-  // @Input() dataSource?: string;
-
   chartOption: EChartsOption = {
-    title: {
-      text: 'CO² Konzentration (ppm)'
-    },
-    xAxis: {
-      type: 'category',
-      // data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
-    },
-    yAxis: {
-      type: 'value',
-    },
+    title: { text: 'CO² Konzentration (ppm)' },
+    xAxis: { type: 'category' },
+    yAxis: { type: 'value' },
 
-    dataZoom: [{
-      startValue: 0
-    }, {
-      type: 'inside'
-    }],
+    dataZoom: [
+      { startValue: 0 },
+      { type: 'inside' }
+    ],
 
     tooltip: {
       trigger: 'item',
@@ -54,135 +42,55 @@ export class LineChartComponent implements OnInit {
     visualMap: {
       top: 10,
       right: 10,
-      pieces: [{
-        gt: 0,
-        lte: 850,
-        color: '#096'
-      }, {
-        gt: 850,
-        lte: 1000,
-        color: 'orange'
-      }, {
-        gt: 1000,
-        color: 'red',
-        // label: 'kritisch (>1000)',
-      }],
-      outOfRange: {
-        color: '#999'
-      }
+      pieces: [
+        { gt: 0, lte: 850, color: '#096' },
+        { gt: 850, lte: 1000, color: 'orange' },
+        { gt: 1000, color: 'red' } // label: 'kritisch (>1000)'
+      ],
+      outOfRange: { color: '#999' }
     },
 
-    series: [
-      {
-        // data: [820, 932, 901, 934, 1290, 1330, 1320],
-        type: 'line',
-        data: [
-          //     ['2019-10-10', 200],
-          //     ['2019-10-11', 400],
-          //     ['2019-10-12', 650],
-          //     ['2019-10-13', 500],
-          //     ['2019-10-14', 250],
-          //     ['2019-10-15', 300],
-          //     ['2019-10-16', 450],
-          //     ['2019-10-17', 300],
-          //     ['2019-10-18', 100]
-        ]
-      },
-    ],
-
+    series: [{
+      type: 'line',
+      data: []
+    }],
   };
 
-  constructor(private service: ChartDataService,
-    private changeDetection: ChangeDetectorRef) {
+  private _chart: any;
+
+  constructor(private _service: ChartDataService) {
   }
 
   ngOnInit(): void {
   }
 
-  onChartInit(ec) {
-    this.echartsInstance = ec;
+  onChartInit(chart: any) {
+    this._chart = chart;
   }
 
   updateData(): void {
-
-    // update
-    // if (this.echartsInstance) {
-    //   console.log('clear data');
-    //   this.echartsInstance.clear();
-    //   this.chartOption = {
-    //     title: {
-    //       text: 'CO² Konzentration (ppm)' + this._dataSource
-    //     },
-    //     xAxis: {
-    //       type: 'category',
-    //     },
-    //     yAxis: {
-    //       type: 'value',
-    //     },
-    //     series: []
-    //   };
-
-    //   }
-
-    // this.chartOption.series = [];
-    // this.chartOption.title.text = this._dataSource;
-
-
-    var subscription = this.service.getAirQualityData(this._dataSource, '2021-01').subscribe(data => {
+    var subscription = this._service.getAirQualityData(this._dataSource, '2021-01').subscribe(data => {
       console.log(`Loaded ${data.length} records for datasource: ${this._dataSource}`);
 
-      // var co2 = {
-      //   type: 'line',
-      //   data: []
-      // };
-      // var temp = {
-      //   type: 'line',
-      //   data: []
-      // };
-
-      var series: any = this.chartOption.series;
-      // series[0].data.cl
-
-
+      var chartData = [];
       data.forEach(function (value) {
         // console.log(`- ${value.timestamp}: ${value.co2Concentration}, ${value.temperature}`);
-        series[0].data.push([value.timestamp, value.co2Concentration]);
-        console.log(`- ${series[0].data.length}`);
-        // co2.data.push([value.timestamp, value.co2Concentration]);
-        // temp.data.push([value.timestamp, value.temperature]);
+        chartData.push([value.timestamp, value.co2Concentration]);
       });
 
-      // this.chartOption = {
-      //   title: {
-      //     text: 'CO² Konzentration (ppm)' + this._dataSource
-      //   },
-      //   xAxis: {
-      //     type: 'category',
-      //   },
-      //   yAxis: {
-      //     type: 'value',
-      //   },
-      //   series: []
-      // };
-
-
-      // var series: any = this.chartOption.series;
-      // series.push(co2);
-      // series.push(temp);
-
-
-      // if (this.echartsInstance) {
-      //   console.log('resize');
-      //   this.echartsInstance.resize();
-      // }
-
-      // this.changeDetection.detectChanges();
-
+      if (this._chart) {
+        this._chart.setOption({
+          title: { text: `CO² Konzentration (ppm) - ${this._dataSource}` },
+          series: [{
+            data: chartData,
+            type: 'line'
+          }]
+        });
+      }
 
       // done
       subscription.unsubscribe();
     });
-
   }
 
 }
